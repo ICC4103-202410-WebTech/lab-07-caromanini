fd07c89bb4899af9d08adf95386269c983354ca9
Chiara Romanini <caromanini@miuandes.cl>
2024-04-26 09:40:37 -0400

    finished validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,16 @@ class Post < ApplicationRecord
     has_many :tags, :through => :post_tags
 
     has_many :reference_posts, class_name: "Post", foreign_key: "main_post_id"
-    belongs_to :main_post, class_name: "Post"
+    belongs_to :main_post, class_name: "Post", optional: true
+
+    before_validation :set_default_published_at
+
+    validates :title, :content, :user_id, presence: {message: "is not present!"}
+    validates :answers_count, numericality: {greater_than_or_equal_to: 0, message: "is invalid! (value less than 0)"}
+    validates :likes_count, numericality: {greater_than_or_equal_to: 0, message: "is invalid! (value less than 0)"}
+
+    private
+    def set_default_published_at
+        self.published_at ||= Time.current
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
     has_many :posts
 
-    validates :name, :email, :password, presence: true
-    validates :email, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP}
-    validates :password, length: {minimum: 6} 
+    validates :name, :email, :password, presence: {message: "is not present!"}
+    validates :email, uniqueness: {message: "is already registered!! Please enter another email"}, 
+                      format: {with: URI::MailTo::EMAIL_REGEXP, message: "invalid format !!"}
+    validates :password, length: {minimum: 6, message: "has to have a minimum length of 6 characters"} 
 end
