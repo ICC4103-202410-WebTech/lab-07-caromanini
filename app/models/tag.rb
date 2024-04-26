78fd172c028ab2d6013b8d465ab2cd152b288d13
Chiara Romanini <caromanini@miuandes.cl>
2024-04-26 09:45:57 -0400

    finished tag validations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,5 @@ class Tag < ApplicationRecord
     has_many :post_tags
     has_many :posts, :through => :post_tags
 
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: {message: "is not present! :o"}, uniqueness: {message: "is not unique!! :("}
 end
