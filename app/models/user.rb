3f04541ea6620ad74a2bf6fa1d0b9a7b39b8f83e
Chiara Romanini <caromanini@miuandes.cl>
2024-04-25 20:40:38 -0400

    user validations done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_many :posts
+
+    validates :name, :email, :password, presence: true
+    validates :email, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP}
+    validates :password, length: {minimum: 6} 
 end
